706526aa714444a720970218b260c91dbe9de6c7
Denis Paluca <denispaluca@gmail.com>
2018-02-01 18:26:17 +0100

    finish 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,21 @@
 #write your code here
+def add(a,b)
+  return a+b
+end
+
+def subtract(a,b)
+  return a-b
+end
+
+def sum(a)
+  if a == [] then
+    return 0
+  end
+
+  h = 0
+  a.each { |i| 
+    h+=i
+  }
+
+  return h
+end
