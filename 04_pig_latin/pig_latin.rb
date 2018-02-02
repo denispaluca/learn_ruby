1e4c83851138100b88e73ede173850d4acfbdf51
Denis Paluca <denispaluca@gmail.com>
2018-02-03 00:07:08 +0100

    finish 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
 #write your code here
+def translate(a)
+  words = a.split
+  vowels = ['a','u','y','i','o','e']
+  words.each.with_index{ |word, i|
+    word.each_char{ |c|
+      if !c.start_with?('a','u','y','i','o','e')
+        word+=c
+        word.sub!(c,'')
+        if c == 'q' and word.start_with?('u')
+          word+='u'
+          word.sub!('u','')
+          puts "hey"
+        end
+      else
+        break
+      end
+      
+    }
+    word+='ay'
+    words[i]=word
+  }
+  return words.join(' ').strip
+end
