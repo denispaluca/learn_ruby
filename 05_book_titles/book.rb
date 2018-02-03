3da6da7195d8829f07e678c899edcb53fbaba76b
Denis Paluca <denispaluca@gmail.com>
2018-02-03 23:56:32 +0100

    finish 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+  def title
+    @title
+  end
+
+  def title=(str)
+    arr = str.split
+    arr.each{|v|
+      if !["an","and","of","in","a","the"].include?(v)
+        v.capitalize!
+      end
+    }
+    arr[0].capitalize!
+    @title = arr.join(' ')
+  end
 end
